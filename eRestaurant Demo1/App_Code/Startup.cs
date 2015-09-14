c52b9c6fa055e9cd376efb2a172d352579fdeac3
Gregory Gould <ggould5@studentmail.nait.ca>
2015-09-14 16:26:56 -0600

    setup of new repo structure and demo VS project

---
+++ eRestaurant Demo1/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eRestaurant_Demo.Startup))]
+namespace eRestaurant_Demo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
